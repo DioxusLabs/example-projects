a69bb4f16c0ef980c8be0bd44feb8b44bb5a10b7
Dirk Stolle <striezel-dev@web.de>
2023-06-24 05:06:32 +0200

    Fix a few typos

--- ecommerce-site/src/components/error.rs
+++ ecommerce-site/src/components/error.rs
@@ -6,7 +6,7 @@ pub fn error_page(cx: Scope) -> Element {
         section { class: "py-20",
             div { class: "container mx-auto px-4",
                 div { class: "flex flex-wrap -mx-4 mb-24 text-center",
-                    "An internal error has occured"
+                    "An internal error has occurred"
                 }
             }
         }
