64ba1215934e28f3e4f3a144e7e858ca6086362c
marc2332 <mespinsanz@gmail.com>
2023-07-13 00:18:40 +0200

    feat: Enhance dog app

--- dog-app/src/models.rs
+++ dog-app/src/models.rs
@@ -4,12 +4,38 @@
 // By sub-breed
 // Browse breed list
 
-pub async fn list_all_breeds() {}
+#![allow(non_snake_case)]
+use std::collections::HashMap;
+use serde::{Deserialize, Serialize};
 
-pub async fn random_image() {}
+#[derive(Serialize, Deserialize)]
+pub struct AllBreeds {
+    pub message: HashMap<String, Vec<String>>,
+    pub status: String,
+}
 
-pub async fn sub_breed() {}
 
-pub async fn breed_list() {}
+pub async fn list_all_breeds() -> reqwest::Result<AllBreeds> {
+    reqwest::get("https://dog.ceo/api/breeds/list/all")
+        .await
+        ?
+        .json::<AllBreeds>()
+        .await
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct RandomImageByBreed {
+    pub message: String,
+    pub status: String,
+}
+
+
+pub async fn random_image_by_breed(breed: &str) -> reqwest::Result<RandomImageByBreed> {
+    reqwest::get(format!("https://dog.ceo/api/breed/{breed}/images/random"))
+    .await
+    ?
+    .json::<RandomImageByBreed>()
+    .await
+}
 
 
